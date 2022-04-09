0ca1d6432150f3f9873f50494d44ec4356bf5148
Shameel Abdul <shameelm@skillspire.net>
2022-04-09 11:58:04 -0500

    cleanup

---
+++ register.js
@@ -0,0 +1,34 @@
+function processForm()
+{
+    let uGpa = document.getElementById("uGpa").value;
+    let selectMern = document.getElementById("selectMern").checked;
+
+    console.log(uGpa);
+    console.log(selectMern);
+
+// Constants - Array or individual variables.
+// Array of fees, array of course
+// Fees
+
+
+// Get all the input paramaters - use document DOM
+// assign this input params to a variable
+
+// Creating new variables which will be result variable
+// Computing the result
+// Total, Tax, Final Amount
+
+// If ( Check whether Array course is checked - Search for the course)
+// compute sum that is you will add to it - Total
+
+// If - If - 5 of them
+
+// Result Courses - Div 
+
+// Result Discounts 
+
+// Result Tax 
+
+// Result Final Amount
+
+}
